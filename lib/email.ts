76b504ad0779e353844709baf18d362e9342a3ac
enesarihan <enessarihan35@icloud.com>
2025-05-12 19:18:36 +0300

    Fix email sender format for consistency

--- lib/email.ts
+++ lib/email.ts
@@ -8,7 +8,7 @@ const resend = new Resend(process.env.RESEND_API_KEY as string);
 export const sendEmail = async (emailFormData: EmailTemplateProps) => {
   try {
     await resend.emails.send({
-      from: `${"Enes SARIHAN"} ${"onboarding@resend.dev"}`,
+      from: `Enes SARIHAN <onboarding@resend.dev>`,
       to: ["enessarihan35@icloud.com"],
       subject: emailFormData.subject,
       react: await EmailTemplate({
